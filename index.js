85126fbb31e38e3703cb8fc2e3ccd03f63992e76
jbassoRMIT <s4101667@student.rmit.edu.au>
2025-10-05 12:03:29 +1100

    Functions implemented, calculator buttons implemented, addEventListeners added to each button to enable numbers to appear in display

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
-console.log("hello world");
+const add=(a,b)=>{
+    return (a+b);
+}
+
+const subtract=(a,b)=>{
+    return (a-b);
+}
+
+const multiply=(a,b)=>{
+    return (a*b);
+}
+
+const divide=(a,b)=>{
+    return (a/b);
+}
+
+const operate=(a,b,operator)=>{
+    if(operator=="+"){
+        return add(a,b);
+    }
+    else if(operator=="-"){
+        return subtract(a,b);
+    }
+    else if(operator=="*"){
+        return multiply(a,b);
+    }
+    if(operator=="/"){
+        return divide(a,b);
+    }
+}
+
+//Create functions that populate display
+//target button elements
+const buttons=document.querySelectorAll("button");
+
+//iterate over the buttons
+for(let button of buttons){
+    //add event Listener to each button on click
+    button.addEventListener("click",()=>{
+        //on each click, target the calculator display div
+        const display=document.querySelector(".calculatorDisplay");
+
+        //create a num as the value of inherently of that button
+        const num=button.textContent;
+
+        //append the num to displayDiv
+        display.textContent+=num;
+    })
+}
