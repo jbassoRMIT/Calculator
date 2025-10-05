05ee6f87f3a2c835f8570f057aea17149ae3eeef
jbassoRMIT <s4101667@student.rmit.edu.au>
2025-10-06 09:52:05 +1100

    Can add 2nd number to screen and results stored in an array, need to clean up equals functionality

--- index.js
+++ index.js
@@ -29,21 +29,72 @@ const operate=(a,b,operator)=>{
     }
 }
 
-//Create functions that populate display
-//target button elements
-const buttons=document.querySelectorAll("button");
 
-//iterate over the buttons
-for(let button of buttons){
-    //add event Listener to each button on click
+    //Create functions that populate display
+    //target number button elements
+    const numberButtons=document.querySelectorAll(".numberButton");
+
+    //create a variable to store the value entered into the screen
+    let value=0;
+
+    //iterate over the number buttons
+    for(let button of numberButtons){
+        //add event Listener to each button on click
+        button.addEventListener("click",()=>{
+            //on each click, target the calculator display div
+            const display=document.querySelector(".calculatorDisplay");
+
+            //create a num as the value of inherently of that button
+            const num=button.textContent;
+
+            //append the num to displayDiv
+            display.textContent+=num;
+
+            //add the numeric value of num to value;
+            value=Number(display.textContent);
+            console.log(value);
+        })
+    }
+
+
+//target operator buttons and add event listeners
+const operatorButtons=document.querySelectorAll(".operatorButton");
+
+//create an array to store the values
+let values=[]
+
+let result=0;
+//on each click, target the calculator display div
+const display=document.querySelector(".calculatorDisplay");
+
+
+//iterate over the operator buttons
+for(let button of operatorButtons){
+    //add event listener
     button.addEventListener("click",()=>{
-        //on each click, target the calculator display div
-        const display=document.querySelector(".calculatorDisplay");
+        //check what type of button it is:
+        const operator=button.textContent;
 
-        //create a num as the value of inherently of that button
-        const num=button.textContent;
+        //we need 2 values to work with any operation, 
+        // we can assume the user has already entered some number in the screen, push that to arrays value, then reset val to 0
+        //Clear the contents of value
+        values.push(value);
+        display.textContent="";
+        value=0;
 
-        //append the num to displayDiv
-        display.textContent+=num;
+        //Can only run the calculator ops if values is of length 2:
+        if(values.length==2){
+            if(operator=="+"){
+                result=add(values[0],values[1]);
+                console.log(result);
+            }
+        }
     })
 }
+
+//target equals button
+const equalsButton=document.querySelector(".equalsButton");
+equalsButton.addEventListener("click",()=>{
+    display.textContent=result;
+    console.log(values);
+})
